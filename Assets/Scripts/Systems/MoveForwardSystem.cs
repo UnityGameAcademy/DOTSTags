ff78bc109aea457925874b6c1cb315f12b2a95b1
Wilmer Lin <unitygameacademy@gmail.com>
2020-04-27 14:42:48 -0700

    Tags, MoveForwardSystem, SpinnerSystem added

---
+++ Assets/Scripts/Systems/MoveForwardSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MoveForwardSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.
+            WithNone<PlayerTag>().
+            WithAll<AsteroidTag>().
+            ForEach((ref Translation pos, in MoveData moveData, in Rotation rot) =>
+        {
+            float3 forwardDirection = math.forward(rot.Value);
+            pos.Value += forwardDirection * moveData.speed * deltaTime;
+
+        }).ScheduleParallel();
+
+
+
+    }
+}
